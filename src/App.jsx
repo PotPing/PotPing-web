c5bed914cce75e301c9ac7439e0add62990e29a6
hyeji-neee <tlsgmlsk123@naver.com>
2025-12-02 22:33:28 +0900

    feat: 랜딩 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
