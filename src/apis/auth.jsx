6d92572d8bfcdfc6561668d7a99c713bc5c47e93
Hyejin <tlsgmlsk123@naver.com>
2025-12-06 02:55:44 +0900

    feat: 로그아웃 API 연동

--- src/apis/auth.jsx
+++ src/apis/auth.jsx
@@ -24,3 +24,9 @@ export const signin = async ({ userId, password }) => {
   const res = await api.post("/api/auth/signin", body);
   return res.data; 
 };
+
+// 로그아웃 
+export const logoutApi = async () => {
+  const res = await api.post("/api/auth/logout");
+  return res.data;
+};
