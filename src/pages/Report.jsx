27ec67c5c768b0ab876db9b94313fd103b10eb6b
Hyejin <tlsgmlsk123@naver.com>
2025-12-06 18:07:38 +0900

    fix: 존재하지 않는 변수 삭제

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -169,7 +169,6 @@ export default function Report() {
         detectedAt,
         message,
         potholeCount,
-        dangerScore,
       });
       setHasResult(true);
       setSessionId(null);
